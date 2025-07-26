d23f201266d1ac9f557ff6e85c1b4bc1c6f85358
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2025-07-26 14:51:09 +0530

    Day-20 Snake Game

---
+++ Snake Game/main.py
@@ -0,0 +1,24 @@
+import time
+from turtle import Turtle, Screen
+from snake import Snake
+
+screen = Screen()
+screen.setup(height=600, width=600)
+screen.bgcolor("black")
+screen.title("Venom - The Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+
+game = True
+while game:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+screen.exitonclick()
