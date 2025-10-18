e6d6cccb2196d91bea86f3687ba044877970c045
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2025-10-18 22:34:07 +0530

    Day-27 advanced tkinter

--- Day-27/main.py
+++ Day-27/main.py
@@ -1,13 +1,26 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("First GUI using tkinter")
 window.minsize(500,300)
 
-my_label = tkinter.Label(text="I am a label.", font=("Aerial", 24, "bold"))
+my_label = Label(text="I am a label.", font=("Aerial", 12, "bold"))
 # my_label.pack()
 # my_label.pack(side="left")
 # my_label.pack(side="bottom")
 my_label.pack(expand=True)
+
+my_label["text"] = "New text"
+my_label.config(text="New Text")
+
+input = Entry(width=10)
+input.pack()
+
+def button_clicked():
+    a = 0
+    my_label.config(text=f"Text: {input.get()}")
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
 
 window.mainloop()
