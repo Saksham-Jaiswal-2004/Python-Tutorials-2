b271f32207da7fa41dbea7414575b325ea02eaf2
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2025-11-27 12:58:05 +0530

    Day-30 1st Lec

---
+++ Day-30/main.py
@@ -0,0 +1,15 @@
+try:
+    file = open("new_file2.txt")
+    test_dict = {"Name": "Saksham"}
+    print(test_dict["age"])
+except FileNotFoundError:
+    file = open("new_file2.txt", "w")
+    print("New File Created")
+except KeyError as error_message:
+    print(f"The requested key {error_message} does not exist!")
+# This else block runs when try block executes successfully
+else:
+    print("This is else block")
+finally:
+    file.close()
+    print("Execution Completed!")
